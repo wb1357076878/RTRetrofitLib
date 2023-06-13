701666cea0639bee0a52e09b351601bc15ae6e44
王波 <1357076878@qq.com>
2023-06-13 11:18:55 +0800

    feat: Perfect the encapsulation of network library

--- app/src/main/java/com/feiniu/rtnetworklib/network/FNBaseResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.feiniu.rtnetworklib.network
-
-data class FNBaseResponse<T> (
-    var code: Int = 0,
-    var msg: String = "",
-    var data: T? = null
-)
--- app/src/main/java/com/feiniu/rtnetworklib/network/FNHandleInfo.kt
+++
@@ -1,3 +0,0 @@
-package com.feiniu.rtnetworklib.network
-
-data class FNHandleInfo(var text: String?)
